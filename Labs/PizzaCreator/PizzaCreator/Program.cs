6eedcbe51619be389c2051486f284e64da9379f0
raulflo <raulflo2012@gmail.com>
2019-02-02 15:15:32 -0600

    Cleaned up Handlechoice

--- Labs/PizzaCreator/PizzaCreator/Program.cs
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -1,6 +1,7 @@
 ﻿/*
-    Lab 1
+    Lab 1 ITSE1430
     Raul Flores
+
 */
 
 using System;
@@ -13,42 +14,75 @@ namespace PizzaCreator
 {
     class Program
     {
-        static void Main( string[] args )
+        private static void HandleMenuOneChoice()
         {
-            DisplayMenuOne();
-            var menuOneChoice = Console.ReadLine();
 
-            while (menuOneChoice != "4")
+            bool userQuit = false;
+
+            while (!userQuit)
             {
+                DisplayMenuOne();
+                var menuOneChoice = Console.ReadLine();
                 switch (menuOneChoice)
                 {
                     case "1":
-                        NewOrder(); 
-                        break;
+                    NewOrder();
+                    break;
                     case "2":
-                        ModifyOrder();
-                        break;
+                    ModifyOrder();
+                    break;
                     case "3":
-                        DisplayOrder();
-                        break;
+                    DisplayOrder();
+                    break;
+                    case "4":
+                    userQuit = true;
+                    break;
+
                     default:
                         {
                             Console.WriteLine("Invalid Entry");
-                            DisplayMenuOne();
                             break;
                         }
                 }
-                menuOneChoice = Console.ReadLine();
+
+
 
             }// end of while loop
+        }
+        static void Main( string[] args )
+        {
+            //DisplayMenuOne();
+            HandleMenuOneChoice();
 
-            
-            
+
+
         }//end of main
 
         private static void NewOrder()
         {
-            throw new NotImplementedException();
+            double sizeOfPizza = SizeOfPizza();
+
+
+            //RedoOrder("Would you like to start a new Order?Y/N");
+        }
+
+        private static double SizeOfPizza()
+        {
+            Console.WriteLine("What size Pizza?\n S=Small \n M=Medium \n L=Large");
+            string result = Console.ReadLine().ToUpper();
+            switch (result)
+            {
+                case "S":
+                return 5.00;
+                case "M":
+                return 6.25;
+                case "L":
+                return 8.75;
+                default:
+                Console.WriteLine("Please pick S,M,L.");
+                return 0;
+
+            }
         }
 
         private static void ModifyOrder()
@@ -61,17 +95,35 @@ namespace PizzaCreator
             throw new NotImplementedException();
         }
 
+
+
         private static void DisplayMenuOne()
         {
             Console.WriteLine("Tccd Pizza");
-            Console.WriteLine("----------");
+            Console.WriteLine("---------- \t\t\t CART:$");
             Console.WriteLine("1.New Order");
             Console.WriteLine("2.Modify Order");
             Console.WriteLine("3.Display Order");
             Console.WriteLine("4.Quit");
 
         }
+        //private static void RedoOrder(string message)
+        //{
+        //    do
+        //    {
+        //        Console.WriteLine(message);
+        //        string result = Console.ReadLine().ToUpper();
 
+        //        //Validate it is a boolean
+        //        if (result == "Y")
+        //            DisplayMenuOne();
+
+        //        if (result == "N")
+
+
+        //        Console.WriteLine("Enter Y or N");
+        //    } while (true);
+        //}
 
 
 
