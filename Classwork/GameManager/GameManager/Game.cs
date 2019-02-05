6e469b69d4c2dbdbd39e123186d7145c502a4087
raul.flores215231 <raul.flores215231@NEITD-123523>
2019-02-04 21:37:30 -0600

    Added GameManager and GameManager.Host.WinForms

---
+++ Classwork/GameManager/GameManager/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManager
+{
+    /// <summary>Represents a game.</summary>
+    public class Game
+    {
+        /// <summary>Name of the Game.</summary>
+       public string Name;
+
+        /// <summary>Publisher of the Game. </summary>
+       public string Publisher;
+       public decimal Price;
+       public bool Owned;
+       public bool Completed;
+    }
+}
