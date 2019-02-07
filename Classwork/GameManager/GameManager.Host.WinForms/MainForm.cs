5fe825b1256873eaae8c1e67cf90acc77da64186
raul.flores215231 <raul.flores215231@NEITD-123523>
2019-02-06 21:25:00 -0600

    Added File,Games, About

--- Classwork/GameManager/GameManager.Host.WinForms/MainForm.cs
+++ Classwork/GameManager/GameManager.Host.WinForms/MainForm.cs
@@ -23,14 +23,29 @@ namespace GameManager.Host.WinForms
         {
             Game game = new Game();
 
-
-
             game.Name = "DOOM";
             game.Price = 59.99M;
+
+            game.Validate();
+
+
+            //var str = game.Publisher; 
+            // Decimal.TryParse("45.99", out game.Price);
 
 
         }
 
+        private void OnFileExit ( object sender, EventArgs e )
+        {
+            // local variable
+           // var x = 10;
 
+            Close();
+        }
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            MessageBox.Show("Help");
+        }
     }
 }
--- Classwork/GameManager/GameManager/Game.cs
+++ Classwork/GameManager/GameManager/Game.cs
@@ -10,12 +10,35 @@ namespace GameManager
     public class Game
     {
         /// <summary>Name of the Game.</summary>
-       public string Name;
+        public string Name = "";
 
         /// <summary>Publisher of the Game. </summary>
-       public string Publisher;
-       public decimal Price;
-       public bool Owned;
-       public bool Completed;
+        public string Publisher = "";
+        public decimal Price;
+        public bool Owned;
+        public bool Completed;
+
+        /// <summary>Validates the object.</summary>
+        /// <returns></returns>
+        public bool Validate( /* Game this */ )
+        {
+            //Redundant dude
+            //var str = this.Name;
+
+            //Name is required
+            if (String.IsNullOrEmpty(Name))
+                return false;
+
+            //Price >= 0
+            if (Price < 0)
+                return false;
+
+            //Only if you need to pass the instance to somebody else
+            //MyType.Foo(this);
+
+            return true;
+        }
+
+
     }
 }
